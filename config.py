6cf254ec2bd5499967ea0af972f593fe18131462
abhishee-ai <abhishekjio377@gmail.com>
2024-08-14 14:56:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -59,9 +59,9 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "api.shareus.io") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "I27XWFzQPGWzGfP9K5jVchYtxpN2") # shortlink api
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/How_To_Open_Linkl") # how to open link 
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "ziplinker.net") # shortlink domain without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "76212ca70f555b89c8845b29db243f4e588590f9") # shortlink api
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/Panchayat_season_3_webserees") # how to open link 
 
 # Website Info:
 WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
